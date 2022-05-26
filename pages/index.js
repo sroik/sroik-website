a64ac8562690be9713c1b2db4f4b9f68bacfef30
sroik <vasili.kazhanouski@gmail.com>
2022-05-26 13:15:41 +0300

    favicon

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,19 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/main.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Sroik</title>
-        <meta name="description" content="Sroik's personal web site" />
-        <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Sroik's Personal Website" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome
+          SROIK
         </h1>
       </main>
     </div>
