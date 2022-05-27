8c2b792a4f81a63baa3107b4ee2d31e6d3c4be8f
sroik <vasili.kazhanouski@gmail.com>
2022-05-27 09:47:43 +0300

    center sroik

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className={styles.name}>
           SROIK
         </h1>
       </main>
