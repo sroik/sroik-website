bfe5417318ae832c075b9417d5aa1ce179a1f4a5
sroik <vasili.kazhanouski@gmail.com>
2022-05-30 18:16:54 +0300

    fixes

--- components/footer.js
+++ components/footer.js
@@ -4,9 +4,9 @@ import SocialLink from './social-link'
 export default function Footer() {
   return (
   <div className={styles.footer}>
-    <SocialLink icon="/instagram-icon.png" name="Instagram" link="https://www.instagram.com/iamsroik/"/>
-    <SocialLink icon="/youtube-icon.png" name="YouTube" link="https://www.youtube.com/channel/UC21xcITB_H73iJBdLFyY1iA"/>
-    <SocialLink icon="/github-icon.png" name="GitHub" link="https://github.com/sroik"/>
+    <SocialLink icon="/social/instagram-color-icon.png" name="Instagram" link="https://www.instagram.com/iamsroik/"/>
+    <SocialLink icon="/social/youtube-color-icon.png" name="YouTube" link="https://www.youtube.com/channel/UC21xcITB_H73iJBdLFyY1iA"/>
+    <SocialLink icon="/social/github-icon.png" name="GitHub" link="https://github.com/sroik"/>
   </div>
   )
 }
